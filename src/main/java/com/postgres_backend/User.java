c9d7d50c11101d13952d372c1f00f3aecc773d15
rrusakov <ruslan.rusakov@telekom.com>
2021-07-21 23:50:59 +0300

    initial commit

---
+++ src/main/java/com/postgres_backend/User.java
@@ -0,0 +1,12 @@
+package com.postgres_backend;
+
+import javax.validation.constraints.NotNull;
+
+public class User {
+
+	@NotNull
+	String firstName;
+
+	String lasName;
+
+}
---
+++ src/test/java/com/postgres_backend/PostgresBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postgres_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
